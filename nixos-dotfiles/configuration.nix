d42a9d3b677ad461e8224502aff866dab78a923d
Henry Hiles <henry@henryhiles.com>
2024-01-13 09:40:55 -0500

    Update configuration.nix

--- nixos-dotfiles/configuration.nix
+++ nixos-dotfiles/configuration.nix
@@ -2,7 +2,7 @@
 # your system.  Help is available in the configuration.nix(5) man page
 # and in the NixOS manual (accessible by running ‘nixos-help’).
 
-{ config, pkgs, ... }:
+{ inputs, pkgs, ... }:
 
 {
   imports =
@@ -111,7 +111,7 @@
       heroic
       krita 
       gimp
-      stable.unityhub
+      inputs.nixpkgs-stable.legacyPackages.${system}.unityhub
     ];
   };
 
