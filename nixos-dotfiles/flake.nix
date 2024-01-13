eb1ecac16c0df0f20c2459e117aca7eb12bda277
ZoeTheTransHoe <103337562+ZoeTheTransHoe@users.noreply.github.com>
2024-01-13 13:28:26 +0000

    First Commit
    Just The NixOS Specific Stuff

---
+++ nixos-dotfiles/flake.nix
@@ -0,0 +1,36 @@
+{
+  description = "Meow :3";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixpkgs-unstable";
+    nixpkgs-stable.url = "github:NixOS/nixpkgs/nixos-23.11";
+    plasma6.url = "github:nix-community/kde2nix";
+  };
+
+  outputs = {self, nixpkgs, nixpkgs-stable, plasma6} @ inputs: let
+    system = "x86_64-linux";
+    overlay-stable = final: prev: {
+        stable = import nixpkgs-stable { inherit system; config.allowUnfree = true; };  # unfree needed for unity....
+      };
+  in {
+    nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
+      inherit system;
+      specialArgs = { inherit inputs; };
+      modules = [
+        ({ config, pkgs, ... }: { nixpkgs.overlays = [ overlay-stable ]; })
+         ./configuration.nix plasma6.nixosModules.default
+      ];
+    };
+
+    formatter.x86_64-linux = nixpkgs.legacyPackages.x86_64-linux.alejandra;
+  };
+    
+  nixConfig = {
+    extra-substituters = [
+      "https://nix-community.cachix.org"
+    ];
+    extra-trusted-public-keys = [
+      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+    ];
+  };
+}
