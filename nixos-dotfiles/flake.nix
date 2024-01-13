d13c171466849e233f6c546d9d1e83dfd0743bf8
Henry Hiles <henry@henryhiles.com>
2024-01-13 09:57:13 -0500

    Update flake.nix

--- nixos-dotfiles/flake.nix
+++ nixos-dotfiles/flake.nix
@@ -7,10 +7,13 @@
     plasma6.url = "github:nix-community/kde2nix";
   };
 
-  outputs = {nixpkgs, plasma6} @ inputs: {
+  outputs = {nixpkgs, nixpkgs-stable, plasma6, ...} @ inputs: let
+    system = "x86_64-linux";
+    stable = import nixpkgs-stable { inherit system; config.allowUnfree = true; };  # unfree needed for unity
+  in {
     nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
-      system = "x86_64-linux";
-      specialArgs = { inherit inputs; };
+      inherit system
+      specialArgs = { inherit inputs stable; };
       modules = [
         ./cachix.nix
         ./configuration.nix
